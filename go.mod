6dd3a975aa8ff8a3eaf564b263939f42cb3c4d2d
kumahr <perso.rahim@gmail.com>
2021-07-24 01:34:00 +0200

    use sarama kafka lib

---
+++ go.mod
@@ -0,0 +1,21 @@
+module kumahr.com/kafka-injector-ws
+
+go 1.16
+
+require (
+	github.com/DataDog/zstd v1.4.8 // indirect
+	github.com/Shopify/toxiproxy v2.1.4+incompatible // indirect
+	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/eapache/go-resiliency v1.2.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
+	github.com/frankban/quicktest v1.11.3 // indirect
+	github.com/gofiber/fiber/v2 v2.15.0
+	github.com/klauspost/compress v1.13.1 // indirect
+	github.com/pierrec/lz4 v2.6.0+incompatible // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/valyala/fasthttp v1.28.0 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	gopkg.in/Shopify/sarama.v1 v1.20.1
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"gopkg.in/Shopify/sarama.v1"
+)
+
+func postMessage(c *fiber.Ctx, writer sarama.SyncProducer) error {
+	topic := c.Params("topic")
+	key := c.Query("key")
+	message := c.Body()
+	event := &sarama.ProducerMessage{Topic: topic, Key: sarama.StringEncoder(key), Value: sarama.StringEncoder(message)}
+	_, _, err := writer.SendMessage(event)
+	if err != nil {
+		return c.Status(400).SendString(err.Error())
+	}
+	return c.SendStatus(201)
+}
+
+func main() {
+	producer, err := sarama.NewSyncProducer([]string{"localhost:9092"}, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		if err := producer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+	app.Post("/topics/:topic", func(c *fiber.Ctx) error {
+		return postMessage(c, producer)
+	})
+	app.Listen(":8585")
+
+}
